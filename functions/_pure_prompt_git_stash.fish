00c8e221d2665faf92070bb16697d5657d5c470a
Chris Barry <chris@greatjones.co>
2020-02-03 16:32:44 -0500

    fix up stash 2

--- functions/_pure_prompt_git_stash.fish
+++ functions/_pure_prompt_git_stash.fish
@@ -1,15 +1,12 @@
 function _pure_prompt_git_stash
     set --local git_stash_symbol
     set --local git_stash_color
+    set --local git_stash_count (count (command git stash list --no-decorate 2>/dev/null))
 
-    set --local has_stashed_files (
-        command git rev-list --walk-reflogs --count refs/stash >/dev/null 2>&1
-        and echo "true"
-    )
-    if test -n "$has_stashed_files" # untracked or un-commited files
-        set git_stash_symbol " $pure_symbol_git_stash"
-        set git_stash_color (_pure_set_color $pure_color_git_stash)
+    if test $git_stash_count -gt 0 # untracked or un-commited files
+        set git_stash_symbol "$pure_symbol_stash"
+        set git_stash_color (_pure_set_color $pure_color_git_unpushed_commits)
     end
 
-    echo "$git_stash_color$git_stash_symbol"
+    echo " $git_stash_color$git_stash_symbol"
 end
