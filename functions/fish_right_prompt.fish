f27569c78a4897f744ba94bc20f5297cb5b91bf2
Chris Barry <chris@greatjones.co>
2020-11-30 09:42:07 -0500

    remove time

--- functions/fish_right_prompt.fish
+++
@@ -1,5 +0,0 @@
-function fish_right_prompt -d 'show the date on the right'
-  set --local system_time (_pure_prompt_system_time)
-  set --local time_color (_pure_set_color $pure_color_system_time)
-  echo "$time_color"(date +%T)
-end
